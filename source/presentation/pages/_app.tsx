d091c42671450f43f2d4078b5b4941c061c16437
Gabriel Jesus <gabriel.ds.jesus@gmail.com>
2023-02-08 10:59:40 -0300

    refactor: :recycle: added font to tailwindcss config
    Added font "Poppins" from google fonts via @next/fonts

--- source/presentation/pages/_app.tsx
+++ source/presentation/pages/_app.tsx
@@ -2,8 +2,29 @@ import React from "react";
 import type { AppProps } from "next/app";
 import "@/presentation/styles/globals.css";
 
+import { Poppins } from "@next/font/google";
+import classNames from "classnames";
+import Head from "next/head";
+
+const poppins = Poppins({
+  subsets: ["latin"],
+  weight: ["400", "700", "800"],
+  variable: "--font-poppins"
+});
+
+
+
 function MyApp({ Component, pageProps }: AppProps): JSX.Element {
-  return <Component {...pageProps} />
+  return (
+    <>
+    <Head>
+
+    </Head>
+    <main className={classNames(poppins.variable, "font-poppins")}>
+      <Component {...pageProps} />
+    </main>
+    </>
+  )
 }
 
 export default MyApp
