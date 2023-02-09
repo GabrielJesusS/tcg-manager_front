8ec12a50e0ca1c2dbe8ead6f069edce8810b475f
Gabriel Jesus <gabriel.ds.jesus@gmail.com>
2023-02-08 23:45:14 -0300

    feat: :sparkles: navbar component - not finished

--- source/presentation/pages/index.tsx
+++ source/presentation/pages/index.tsx
@@ -1,8 +1,10 @@
 import { Footer } from "@/presentation/components/common/Footer";
+import { Navbar } from "@/presentation/components/common/Navbar";
 
 export default function Home(): JSX.Element {
   return (
     <>
+      <Navbar></Navbar>
       <Footer></Footer>
     </>
   );
