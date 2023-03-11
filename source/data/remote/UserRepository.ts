2925a53cd7d190647191b77d1b935525da639beb
Gabriel Jesus <gabrieljesus9221@gmail.com>
2023-03-11 14:36:09 -0300

    refactor: implement cookie service on usecase

--- source/data/remote/UserRepository.ts
+++ source/data/remote/UserRepository.ts
@@ -5,6 +5,7 @@ import { HttpMethod, IHttpClient } from "@/services/http";
 import { IApiResponse } from "../modules/IApiResponse";
 import { IUserCreatePayload } from "./models/CreateUserPayload";
 import {generateHttpErrorResponse } from "@/data/modules/generateHttpErrorResponse"
+import { ICookieService } from "@/services/ICookieService";
 
 interface CreateParams {
   userName: string;
@@ -15,14 +16,14 @@ interface CreateParams {
 export class UserRepository implements IUserRepository {
   private readonly client: IHttpClient;
 
-  private readonly cookieService: string; //TODO:change string to ICookieService when implemented
+  private readonly cookieService: ICookieService;
 
   //Routes
   private static readonly createRoute: string = "/user/register";
 
   constructor(
     client: IHttpClient,
-    cookieService: string //TODO:change string to ICookieService when implemented
+    cookieService: ICookieService //TODO:change string to ICookieService when implemented
   ) {
     this.client = client;
     this.cookieService = cookieService;
--- source/factories/createRegisterUserUsecase.ts
+++ source/factories/createRegisterUserUsecase.ts
@@ -1,11 +1,12 @@
 import { TFactory } from "@/core/Factory";
 import { UserRepository } from "@/data/remote/UserRepository";
 import { RegisterUserUsecase } from "@/domain/usecases/RegisterUserUsecase";
+import { CookieServiceJsCookie } from "@/infra/CookieServiceJsCookie";
 import { HttpClientAxios } from "@/infra/HttpClientAxios";
 
 export const createRegisterUserUsecase: TFactory<RegisterUserUsecase> = () => {
   const client = new HttpClientAxios();
-  const cookieService = "";
+  const cookieService = new CookieServiceJsCookie();
 
   const userRepository = new UserRepository(client, cookieService);
 
