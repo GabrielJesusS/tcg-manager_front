fc3ac283f75c3a8848a5d3687b646dd7e1b9b06f
Gabriel Jesus <gabrieljesus9221@gmail.com>
2023-04-13 09:39:49 -0300

    refactor: dropdown list component

--- source/presentation/components/common/Dropdown.tsx
+++ source/presentation/components/common/Dropdown.tsx
@@ -1,49 +1,43 @@
-import { InputHTMLAttributes, SelectHTMLAttributes } from "react";
+import { Listbox } from "@headlessui/react";
+import { ChangeEvent, InputHTMLAttributes, SelectHTMLAttributes, useState } from "react";
+import ArrowIcon from "@/presentation/public/images/icons/chevron.svg";
 
-interface IDropdown {
-  selectPlaceholder: string;
-  label: string;
-  inputProps?: SelectHTMLAttributes<HTMLSelectElement> & {
-    ref?: React.Ref<HTMLSelectElement>;
-  };
-  options: {
+interface IOption{
+ 
+    id: string | number;
     text: string;
     value: string | number;
-  }[];
+  
 }
 
-export const Dropdown = ({
-  selectPlaceholder,
-  options,
-  label,
-  inputProps,
-}: IDropdown) => {
+interface IDropdown {
+  placeholder: string;
+  label?: string;
+  selectedOption?: string;
+  setter: (e:string)=> void;
+  options: IOption[];
+}
+
+export const Dropdown = ({ placeholder, options, label, selectedOption, setter}: IDropdown) => {
+
   return (
-    <label className="block">
-      <span className="block font-semibold text-lg">{label}</span>
-      <select
-        {...inputProps}
-        defaultValue={"0"}
-        className="w-full text-base md:text-lg text-system px-2 py-1 transition-all cursor-pointer duration-150 hover:bg-secondary-light  bg-secondary outline-none rounded"
+    <Listbox value={selectedOption} onChange={setter}>
+      <Listbox.Label className="mb-3 font-bold">{label}</Listbox.Label>
+      <Listbox.Button
+        className={
+          "text-center flex items-center justify-between w-full text-base border-2 font-medium disabled:bg-system-200 hover:disabled:bg-system-200 disabled:border-system-200 px-4 py-1 rounded-lg drop-shadow-md text-system bg-secondary border-secondary"
+        }
       >
-        <option
-          disabled
-          hidden
-          className="bg-system text-system-800 hover:text-system active:bg-secondary-dark"
-          value="0"
-        >
-          {selectPlaceholder}
-        </option>
+        {selectedOption ? options.find((i=> i.value ===  selectedOption))?.text : placeholder}
+        <ArrowIcon className="w-6 h-6 fill-system rotate-90"/>
+      </Listbox.Button>
+      <Listbox.Options className="py-1 drop-shadow-md bg-system mt-2">
         {options.map((opt) => (
-          <option
-            key={opt.value}
-            className="bg-system text-system-800 hover:text-system active:bg-secondary-dark"
-            value={opt.value}
-          >
+          <Listbox.Option key={opt.id} value={opt.value} className="cursor-pointer px-8 py-2 hover:bg-secondary/25">
             {opt.text}
-          </option>
+          </Listbox.Option>
         ))}
-      </select>
-    </label>
+      </Listbox.Options>
+    </Listbox>
   );
 };
--- source/presentation/pages/pasta/teste.tsx
+++ source/presentation/pages/pasta/teste.tsx
@@ -8,27 +8,31 @@ import { CardSkeleton } from "../../components/common/skeletons/CardSkeleton";
 import { CommentItems } from "../../data/mocks/commentMock";
 import { cardPaginationAtom } from "../../store/paginations";
 import { Breadcrumb } from "../../components/common/Breadcrumb";
+import { useState } from "react";
 
 export default function x() {
-
+  const [data, setData] = useState("")
 
   const {query} = useRouter();
  
+  console.log(data)
   
 
   return (
     <>
       <Dropdown
-        label="dwda"
-        selectPlaceholder="Selecione 1"
-        options={[{ text: "opt1", value: "222" }]}
+        label="Itens"
+        setter={setData}
+        selectedOption={data}
+        placeholder="Selecione um deck"
+        options={[{ id: 1,text: "opt1", value: "222" }]}
       ></Dropdown>
       <Radioinput radioName="Nota do deck" optionsQtd={5}></Radioinput>
      
      <Breadcrumb></Breadcrumb>
 
       <div className="flex p-safe space-x-6">
-
+        <button className="btn btn-primary" type="button">ola sou um botão</button>
       </div>
     </>
   );
