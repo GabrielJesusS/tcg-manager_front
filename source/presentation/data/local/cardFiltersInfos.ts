dbd008510ff971a8e5f83b2ec59e7ece704c7738
Gabriel Jesus <gabrieljesus9221@gmail.com>
2023-04-22 16:30:07 -0300

    feat: adds filters infos

---
+++ source/presentation/data/local/cardFiltersInfos.ts
@@ -0,0 +1,75 @@
+export const typeFilter = [
+    {
+        id: "00",
+        text: "Colorless",
+        value: "Colorless"
+    },
+    {
+        id: "01",
+        text: "Darkness",
+        value: "Darkness"
+    },
+    {
+        id: "02",
+        text: "Dragon",
+        value: "Dragon"
+    },
+    {
+        id: "03",
+        text: "Fairy",
+        value: "Fairy"
+    },
+    {
+        id: "04",
+        text: "Fighting",
+        value: "Fighting"
+    },
+    {
+        id: "05",
+        text: "Fire",
+        value: "Fire"
+    },
+    {
+        id: "06",
+        text: "Grass",
+        value: "Grass"
+    },
+    {
+        id: "07",
+        text: "Lightning",
+        value: "Lightning"
+    },
+    {
+        id: "08",
+        text: "Metal",
+        value: "Metal"
+    },
+    {
+        id: "09",
+        text: "Psychic",
+        value: "Psychic"
+    },
+    {
+        id: "10",
+        text: "Water",
+        value: "water"
+    }
+]
+
+
+
+export const supertypeFilter = [ {
+    id: "00",
+    text: "Energy",
+    value: "Energy"
+},
+{
+    id: "01",
+    text: "Pokémon",
+    value: "Pokémon"
+},
+{
+    id: "02",
+    text: "Trainer",
+    value: "Trainer"
+}];
