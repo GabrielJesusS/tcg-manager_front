6e5963885088b2fd280cfab4e4bf982a45d46d09
Gabriel Jesus <gabrieljesus9221@gmail.com>
2023-04-30 09:09:32 -0300

    refactor: c hanges text input fontsize

--- source/presentation/components/common/Textinput.tsx
+++ source/presentation/components/common/Textinput.tsx
@@ -3,7 +3,7 @@ import { HTMLAttributes, InputHTMLAttributes } from "react";
 
 interface TextinputProps extends HTMLAttributes<HTMLLabelElement>{
     label?: string
-    type: "text" | "tel" | "password" | "email" | "textarea" | "date"
+    type: "text" | "tel" | "password" | "email" | "textarea" | "date" | "url"
     inputProps?: InputHTMLAttributes<HTMLInputElement> & {
         ref?: React.Ref<HTMLInputElement>;
     };
@@ -15,7 +15,7 @@ export const Textinput = ({label, type, placeholder, inputProps, className}:Text
 
     return(
         <label className="block">
-            <span className="block font-semibold text-lg">{label}</span>
+            <span className="block font-semibold text-base">{label}</span>
             <input placeholder={placeholder} className={classNames("w-full drop-shadow-md text-base text-system-800 font-medium bg-system px-3 py-1 transition-all hover:border-system-200 duration-150 border-system-600 focus:border-secondary outline-0 border-2 rounded-full", className)} {...inputProps} type={type}/>
         </label>
     );
