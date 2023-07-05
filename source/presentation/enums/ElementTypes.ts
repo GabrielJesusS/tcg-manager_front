db5807c9e8099937a908651edbecf868be5a90d7
Gabriel Jesus <gabrieljesus9221@gmail.com>
2023-07-04 21:37:09 -0300

    feat: adds text element types

---
+++ source/presentation/enums/ElementTypes.ts
@@ -0,0 +1,8 @@
+export enum ELEMENT_TYPES_ENUM {
+  PARAGRAPH = "paragraph",
+  SMALL = "small",
+  HEADING_ONE = "headingOne",
+  HEADING_TWO = "headingTwo",
+  SUBHEADING = "subheading",
+  LINK = "link"
+}
