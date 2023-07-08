379467bf81879c524cc046048e5d1744376852af
Gabriel Jesus <gabrieljesus9221@gmail.com>
2023-07-08 13:30:14 -0300

    feat: adds use render leafs hook

---
+++ source/presentation/hooks/richTextEditor/useRenderLeafs.tsx
@@ -0,0 +1,23 @@
+import { COLOR_TEXT_MAP } from "@/presentation/enums/ColorEnum";
+import classNames from "classnames";
+import { useCallback } from "react";
+import { RenderLeafProps } from "slate-react";
+
+export function useRenderLeafs() {
+  
+  const renderLeafs = useCallback((props: RenderLeafProps) => {
+    return (
+      <span
+        className={classNames({
+          "font-bold": props.leaf.isBold,
+          "italic": props.leaf.isItalic,
+          "underline underline-offset-2": props.leaf.isUnderline,
+        },  COLOR_TEXT_MAP[props.leaf.color])}
+        {...props.attributes}
+      >
+        {props.children}
+      </span>
+    );
+  }, []);
+  return { renderLeafs};
+}
