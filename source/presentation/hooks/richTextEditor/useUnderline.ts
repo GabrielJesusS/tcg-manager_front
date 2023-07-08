efb8c287da89158afd3d3b515c80c6f697c37b6c
Gabriel Jesus <gabrieljesus9221@gmail.com>
2023-07-08 14:38:26 -0300

    refactor: improves underline rich text

--- source/presentation/hooks/richTextEditor/useUnderline.ts
+++ source/presentation/hooks/richTextEditor/useUnderline.ts
@@ -1,5 +1,7 @@
+import { ColorEnum } from "@/presentation/enums/ColorEnum";
 import { useCallback, useMemo } from "react";
-import { Editor, Text, Transforms } from "slate";
+import { Editor, Range, Text, Transforms } from "slate";
+import { ReactEditor } from "slate-react";
 
 interface IUseUnderline {
   toggleUnderline: () => void;
@@ -9,11 +11,30 @@ interface IUseUnderline {
 export function useUnderline(editor: Editor): IUseUnderline {
   function toggleUnderline(): void {
     const isActive = checkIsUnderline;
-    Transforms.setNodes(
-      editor,
-      { isUnderline: isActive ? undefined : true },
-      { match: (n) => Text.isText(n), split: true }
-    );
+
+    const { selection } = editor;
+    ReactEditor.focus(editor);
+    if (!!selection) {
+      if (Range.isCollapsed(selection)) {
+        Transforms.select(editor, {
+          path: selection.anchor.path,
+          offset: selection.anchor.offset,
+        });
+        Editor.addMark(editor, "isUnderline", isActive ? undefined : true);
+        return;
+      }
+
+      Transforms.setNodes(
+        editor,
+        { isUnderline: isActive ? undefined : true },
+        { match: (n) => Text.isText(n), split: true }
+      );
+
+      Transforms.select(editor, Editor.end(editor, []));
+
+      return;
+    }
+    Transforms.insertNodes(editor, [{ text: "", color: ColorEnum.BASE, isUnderline: true}]);
   }
 
   const checkIsUnderline = useMemo(() => {
