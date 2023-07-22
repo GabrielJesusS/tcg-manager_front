a5cf3d50c506c6396465758c73128e293f161c46
Gabriel Jesus <gabrieljesus9221@gmail.com>
2023-07-21 22:01:01 -0300

    feat: adds check image url utils

---
+++ source/presentation/utils/checkImageUrl.ts
@@ -0,0 +1,5 @@
+export function checkImageUrl(url: string): boolean {
+  return /https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()!@:%_\+.~#?&\/\/=]*)/.test(
+    url
+  );
+}
