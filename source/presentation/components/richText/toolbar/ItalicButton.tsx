f00a0a5375fbe956c654c488f0025e157ec6ee07
Gabriel Jesus <gabrieljesus9221@gmail.com>
2023-07-26 13:21:47 -0300

    feat: improves italic functionality

--- source/presentation/components/richText/toolbar/ItalicButton.tsx
+++ source/presentation/components/richText/toolbar/ItalicButton.tsx
@@ -1,16 +1,21 @@
 import ItalicIcon from "@/presentation/public/images/icons/editor/italic.svg";
-import { Editor } from "slate";
 import { ButtonBase } from "./ButtonBase";
-import { useItalic } from "@/presentation/hooks/richTextEditor/useItalic";
+import { useSlateStatic } from "slate-react";
+import { toggleMarks } from "@/presentation/utils/editor/toggleMarks";
+import { checkMarkIsActive } from "@/presentation/utils/editor/checkMarkIsActive";
 
-interface IItalicButton {
-  editor: Editor;
-}
+export const ItalicButton = (): JSX.Element => {
+  const editor = useSlateStatic();
 
-export const ItalicButton = ({ editor }: IItalicButton) => {
-  const { checkIsItalic, toggleItalic } = useItalic(editor);
+  function handleClick(): void {
+    toggleMarks(editor, "isItalic");
+  }
+
   return (
-    <ButtonBase onClick={toggleItalic} active={checkIsItalic}>
+    <ButtonBase
+      onClick={handleClick}
+      active={checkMarkIsActive(editor, "isItalic")}
+    >
       <ItalicIcon className="h-6" />
     </ButtonBase>
   );
