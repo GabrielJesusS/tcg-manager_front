417cdd2d0eea73a2821d2155fd1d05de45f8939e
Gabriel Jesus <gabrieljesus9221@gmail.com>
2023-07-26 13:29:44 -0300

    feat: improves link modal

--- source/presentation/components/common/modals/LinkEditModal.tsx
+++ source/presentation/components/common/modals/LinkEditModal.tsx
@@ -1,32 +1,26 @@
 import { useRecoilState } from "recoil";
 import { DefaultQuestionModal } from "./DefaultQuestionModal";
-import {
-  linkModalAtom,
-  linkURLAtom,
-} from "@/presentation/store/editor/linkModalAtom";
-import { useSelected, useSlate } from "slate-react";
+import { linkModalAtom } from "@/presentation/store/editor/linkModalAtom";
+import { useSlate } from "slate-react";
 import { useState } from "react";
-import { Transforms } from "slate";
 import { useLink } from "@/presentation/hooks/richTextEditor/useLink";
 
-export const LinkEditModal = () => {
+export const LinkEditModal = ():JSX.Element => {
   const [open, setOpen] = useRecoilState(linkModalAtom);
   const [i, setI] = useState("");
   const editor = useSlate();
-  const { insertLink } = useLink(editor)
+  const { insertLink } = useLink(editor);
 
-
-
-  function handleClick(){
-    insertLink(editor, i)
-    setOpen(false)
+  function handleClick():void {
+    insertLink(editor, i);
+    setOpen(false);
   }
 
   return (
     <DefaultQuestionModal
       isOpen={open}
       close={setOpen}
-      action={{ actionText: "Salvar link", actionClick: handleClick}}
+      action={{ actionText: "Salvar link", actionClick: handleClick }}
     >
       <div>Opan meu amigo, salve salve KKK </div>
       <div>{i}</div>
