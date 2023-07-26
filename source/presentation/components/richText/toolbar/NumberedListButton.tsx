b41376c266a57004d5d4c04d71958896d1e85e58
Gabriel Jesus <gabrieljesus9221@gmail.com>
2023-07-26 13:28:51 -0300

    feat: adds list buttons

--- source/presentation/components/richText/toolbar/NumberedListButton.tsx
+++ source/presentation/components/richText/toolbar/NumberedListButton.tsx
@@ -1,31 +1,23 @@
 import ListIcon from "@/presentation/public/images/icons/editor/list-bulleted.svg";
 import { Editor, Element, Transforms } from "slate";
 import { ButtonBase } from "./ButtonBase";
-import { ELEMENT_TYPES_ENUM, LIST_ELEMENTS } from "@/presentation/enums/ElementTypes";
+import { ELEMENT_TYPES_ENUM } from "@/presentation/enums/ElementTypes";
 import { AlignmentEnum } from "@/presentation/enums/AlignmentEnum";
 import { ListItemElement } from "@/presentation/@types/slate";
-import { ReactEditor, useSlateStatic } from "slate-react";
+import { useSlateStatic } from "slate-react";
 
-export const ListButton = (): JSX.Element => {
+export const NumberedListButton = (): JSX.Element => {
   const editor = useSlateStatic();
 
   function toggleList(): void {
     const { selection } = editor;
-
-    ReactEditor.focus(editor);
 
     if (selection) {
       const [parentNode] = Editor.parent(editor, selection.focus);
 
       if (Element.isElement(parentNode)) {
         if (Element.isElementType(parentNode, ELEMENT_TYPES_ENUM.LIST_ITEM)) {
-          Transforms.unwrapNodes(editor, {
-            match: n =>
-              !Editor.isEditor(n) &&
-              Element.isElement(n) &&
-              LIST_ELEMENTS.includes(n.type),
-            split: true,
-          })
+          Transforms.unwrapNodes(editor);
           Transforms.setNodes(editor, { type: ELEMENT_TYPES_ENUM.PARAGRAPH });
           return;
         }
@@ -34,7 +26,7 @@ export const ListButton = (): JSX.Element => {
           type: ELEMENT_TYPES_ENUM.LIST_ITEM,
         } as Partial<ListItemElement>);
         Transforms.wrapNodes(editor, {
-          type: ELEMENT_TYPES_ENUM.LIST,
+          type: ELEMENT_TYPES_ENUM.NUMBERED_LIST,
           children: [],
           alignment: AlignmentEnum.LEFT,
         });
