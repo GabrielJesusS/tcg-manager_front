4d8b16e6f89bc5cc321e69101214018552bf899d
Gabriel Jesus <gabrieljesus9221@gmail.com>
2023-07-26 13:14:31 -0300

    feat: adds list types to rich text editor

--- source/presentation/@types/slate.d.ts
+++ source/presentation/@types/slate.d.ts
@@ -1,4 +1,4 @@
-import { BaseEditor, Descendant } from "slate";
+import { BaseEditor } from "slate";
 import { ReactEditor } from "slate-react";
 import { ELEMENT_TYPES_ENUM } from "../enums/ElementTypes";
 import { ColorEnum } from "../enums/ColorEnum";
@@ -53,6 +53,18 @@ export interface ListElement {
   alignment: AlignmentEnum;
 }
 
+export interface NumberedListElement {
+  type: ELEMENT_TYPES_ENUM.NUMBERED_LIST;
+  children: CustomText[];
+  alignment: AlignmentEnum;
+}
+
+export interface ListItemElement {
+  type: ELEMENT_TYPES_ENUM.LIST_ITEM;
+  children: CustomText[];
+  alignment: AlignmentEnum;
+}
+
 export interface ImageElement {
   type: ELEMENT_TYPES_ENUM.IMAGE;
   children: CustomText[];
@@ -72,13 +84,16 @@ type CustomElement =
   | QuoteElement
   | LinkElement
   | ListElement
-  | ImageElement;
+  | ImageElement
+  | NumberedListElement
+  | ListItemElement;
 
 interface CustomText {
   text: string;
   isBold?: boolean;
   isItalic?: boolean;
   isUnderline?: boolean;
+  isLink?:boolean;
   color: ColorEnum;
 }
 
