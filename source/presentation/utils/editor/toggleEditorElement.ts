de76fdf3d5655a0272a85f63718dad70aac2ce95
Gabriel Jesus <gabrieljesus9221@gmail.com>
2023-07-26 13:16:07 -0300

    feat: adds toggle element

---
+++ source/presentation/utils/editor/toggleEditorElement.ts
@@ -0,0 +1,43 @@
+import {
+  ELEMENT_TYPES_ENUM,
+  LIST_ELEMENTS,
+} from "@/presentation/enums/ElementTypes";
+import { Editor, Element, Transforms } from "slate";
+import { checkElementIsActive } from "./checkElementIsActive";
+import { CustomElement } from "@/presentation/@types/slate";
+import { AlignmentEnum } from "@/presentation/enums/AlignmentEnum";
+
+export function toggleEditorElement(
+  editor: Editor,
+  block: ELEMENT_TYPES_ENUM
+): void {
+  const isActive = checkElementIsActive(editor, block);
+
+  const isList = LIST_ELEMENTS.includes(block);
+
+  Transforms.unwrapNodes(editor, {
+    match: (n) =>
+      !Editor.isEditor(n) &&
+      Element.isElement(n) &&
+      LIST_ELEMENTS.includes(n.type),
+    split: true,
+  });
+
+  const newType = isActive
+    ? ELEMENT_TYPES_ENUM.PARAGRAPH
+    : isList
+    ? ELEMENT_TYPES_ENUM.LIST_ITEM
+    : block;
+
+  Transforms.setNodes(editor, { type: newType } as Partial<CustomElement>);
+
+  if (!isActive && isList) {
+    const newElement = {
+      type: block,
+      children: [],
+      alignment: AlignmentEnum.LEFT,
+    };
+
+    Transforms.wrapNodes(editor, newElement as CustomElement);
+  }
+}
