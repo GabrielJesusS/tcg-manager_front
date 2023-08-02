e82ac43522bf3805beda26f9173ba05568383f8f
Gabriel Jesus <gabrieljesus9221@gmail.com>
2023-08-02 10:57:26 -0300

    feat: adds key handler utils

---
+++ source/presentation/utils/editor/keyHandler.ts
@@ -0,0 +1,50 @@
+import isHotkey from "is-hotkey";
+import { toggleMarks } from "./toggleMarks";
+import { toggleEditorElement } from "./toggleEditorElement";
+import { Editor, Element } from "slate";
+import { ELEMENT_TYPES_ENUM } from "@/presentation/enums/ElementTypes";
+import { KeyboardEvent } from "react";
+
+export function keyHandler(
+  editor: Editor
+): (e: KeyboardEvent<HTMLDivElement>) => void {
+  return (e: KeyboardEvent<HTMLDivElement>): void => {
+    if (isHotkey("mod+b", e)) {
+      toggleMarks(editor, "isBold");
+    }
+
+    if (isHotkey("mod+i", e)) {
+      toggleMarks(editor, "isItalic");
+    }
+
+    if (isHotkey("mod+u", e)) {
+      toggleMarks(editor, "isUnderline");
+    }
+
+    if (isHotkey("mod+q", e)) {
+      toggleEditorElement(editor, ELEMENT_TYPES_ENUM.QUOTES);
+    }
+
+    if (isHotkey("mod+alt+l", e)) {
+      toggleEditorElement(editor, ELEMENT_TYPES_ENUM.LIST);
+    }
+
+    if (isHotkey("mod+shift+l", e)) {
+      toggleEditorElement(editor, ELEMENT_TYPES_ENUM.NUMBERED_LIST);
+    }
+
+    if (e.key === "Backspace") {
+      const prev = Editor.previous(editor, {
+        at: editor.selection?.focus,
+      });
+
+      if (!prev) return;
+
+      const [prevNode] = prev;
+
+      if (Element.isElementType(prevNode, ELEMENT_TYPES_ENUM.IMAGE)) {
+        e.preventDefault();
+      }
+    }
+  };
+}
