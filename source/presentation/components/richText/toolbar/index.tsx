e3d1a8f55df35eb52a515bba890aaf3331a23733
Gabriel Jesus <gabrieljesus9221@gmail.com>
2023-08-06 16:29:05 -0300

    refactor: adjusts toolbar position

--- source/presentation/components/richText/toolbar/index.tsx
+++ source/presentation/components/richText/toolbar/index.tsx
@@ -12,7 +12,7 @@ import { NumberedListButton } from "./NumberedListButton";
 
 export const ToolBar = (): JSX.Element => {
   return (
-    <div className="bg-system-100 h-fit py-4 rounded-lg md:py-1 w-full flex flex-col md:flex-row md:space-x-8 px-safe md:space-y-0 space-y-2 pb-4 md:pb-1 md:rounded-full sticky top-16 z-20">
+    <div className="bg-system-100 h-fit py-4 rounded-lg md:py-1 w-full flex flex-col md:flex-row md:space-x-8 px-safe md:space-y-0 space-y-2 pb-4 md:pb-1 md:rounded-full sticky top-32 md:top-16 z-20">
       <TextLevelSelector />
       <div className="md:space-x-2 grid grid-cols-4 sm:grid-cols-6 md:flex gap-4 md:flex-wrap">
         <BoldButton />
