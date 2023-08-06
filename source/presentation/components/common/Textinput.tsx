6697de51b6bcd52d802ee09bb8e4d5d49d4809e3
Gabriel Jesus <gabrieljesus9221@gmail.com>
2023-08-06 16:16:16 -0300

    refactor: adjusts text input component

--- source/presentation/components/common/Textinput.tsx
+++ source/presentation/components/common/Textinput.tsx
@@ -1,22 +1,33 @@
 import classNames from "classnames";
-import { HTMLAttributes, InputHTMLAttributes } from "react";
+import type { HTMLAttributes, InputHTMLAttributes } from "react";
 
-interface TextinputProps extends HTMLAttributes<HTMLLabelElement>{
-    label?: string
-    type: "text" | "tel" | "password" | "email" | "textarea" | "date" | "url"
-    inputProps?: InputHTMLAttributes<HTMLInputElement> & {
-        ref?: React.Ref<HTMLInputElement>;
-    };
+interface TextinputProps extends HTMLAttributes<HTMLLabelElement> {
+  label?: string;
+  type: "text" | "tel" | "password" | "email" | "textarea" | "date" | "url";
+  inputProps?: InputHTMLAttributes<HTMLInputElement> & {
+    ref?: React.Ref<HTMLInputElement>;
+  };
 }
 
-
-export const Textinput = ({label, type, placeholder, inputProps, className}:TextinputProps) =>{
-
-
-    return(
-        <label className="block">
-            <span className="block font-semibold text-base">{label}</span>
-            <input placeholder={placeholder} className={classNames("w-full drop-shadow-md text-base text-system-800 font-medium bg-system px-3 py-1 transition-all hover:border-system-200 duration-150 border-system-600 focus:border-secondary outline-0 border-2 rounded-full", className)} {...inputProps} type={type}/>
-        </label>
-    );
-}
+export const Textinput = ({
+  label,
+  type,
+  placeholder,
+  inputProps,
+  className,
+}: TextinputProps): JSX.Element => {
+  return (
+    <label className="block">
+      <span className="block text-base">{label}</span>
+      <input
+        placeholder={placeholder}
+        className={classNames(
+          "w-full drop-shadow-md text-base text-system-800 font-medium bg-system px-3 py-1 transition-all hover:border-system-200 duration-150 border-system-200 focus:border-secondary outline-0 border-2 rounded-full",
+          className
+        )}
+        {...inputProps}
+        type={type}
+      />
+    </label>
+  );
+};
