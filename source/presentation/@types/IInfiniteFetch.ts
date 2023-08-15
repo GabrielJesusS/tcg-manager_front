f1ebc94633e4aac3cd275149e8476f262adeaa60
Gabriel Jesus <gabrieljesus9221@gmail.com>
2023-08-15 20:33:05 -0300

    feat: adds infinite fetch interface

---
+++ source/presentation/@types/IInfiniteFetch.ts
@@ -0,0 +1,10 @@
+import { TApplicationError } from "@/core/Errors";
+
+export interface IInfiniteFetch<T> {
+  data?: T[];
+  size: number;
+  setSize: (val: number | ((x: number) => number)) => void;
+  mutate?: () => void;
+  error?: TApplicationError;
+  isValidating?: boolean;
+}
