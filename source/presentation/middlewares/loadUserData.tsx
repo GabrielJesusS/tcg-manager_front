c682b87c138cb6b30f075963ba1a70c354929c8d
Gabriel Jesus <gabrieljesus9221@gmail.com>
2023-08-15 20:34:36 -0300

    refactor: improves load user data hoc

--- source/presentation/middlewares/loadUserData.tsx
+++ source/presentation/middlewares/loadUserData.tsx
@@ -1,24 +1,20 @@
 import { FC, useEffect } from "react";
 import { useGetProfile } from "../hooks/useGetProfile";
 import { userDataAtom } from "../store/genericAtoms";
-import { useRecoilState } from "recoil";
+import { useSetRecoilState } from "recoil";
 
 export const loadUserData = (Component: FC): FC => {
   const comp = (): JSX.Element => {
+    const setUserData = useSetRecoilState(userDataAtom);
+    const { data, error } = useGetProfile();
 
-    const [userData , setUserData ]= useRecoilState(userDataAtom);
-    const { data, error, isLoading, update} = useGetProfile();
+    useEffect(() => {
+      if (data) {
+        setUserData({ ...data });
+      }
+    }, [data, error]);
 
-
-    useEffect(()=>{
-
-        if(data){
-            setUserData({...data, })
-        }
-
-    },[data, error])
-    
-    return <Component/>;
+    return <Component />;
   };
 
   return comp;
