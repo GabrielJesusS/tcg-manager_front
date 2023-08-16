3a8424ace43e5cb907871d0b919127f48a207380
Gabriel Jesus <gabrieljesus9221@gmail.com>
2023-08-16 11:53:14 -0300

    refactor: adjusts bread crumb

--- source/presentation/components/common/Breadcrumb.tsx
+++ source/presentation/components/common/Breadcrumb.tsx
@@ -28,14 +28,14 @@ export const Breadcrumb = (): JSX.Element => {
   }, [asPath]);
 
   return (
-    <div>
-      <ol className="fill-system-400 font-bold text-system-400 flex items-end">
+    <div className="absolute z-10 left-6 top-20 hidden md:block">
+      <ol className="font-bold text-system-200 flex items-end">
         <li>
           <Link
             href={PageRoutesEnum.HOME}
             className="flex w-fit items-end space-x-1 hover:text-secondary hover:fill-secondary transition-all duration-150 ease-in-out"
           >
-            <HomeIcon className="h-6 w-6 fill-inherit" />
+            <HomeIcon className="h-6 w-6 fill-current" />
             <span className="block leading-none">home</span>
           </Link>
         </li>
@@ -45,10 +45,10 @@ export const Breadcrumb = (): JSX.Element => {
               href={path.path}
               className="flex items-end hover:text-secondary"
             >
-              <ArrowIcon className="h-4 w-4 fill-inherit" />
+              <ArrowIcon className="h-4 w-4 fill-current" />
               <span
-                className={classNames("block leading-none", {
-                  "text-system-800 hover:text-secondary":
+                className={classNames("block leading-none border-b-2 border-spacing-2 border-secondary", {
+                  "text-system hover:text-secondary":
                     path.path === pathname,
                 })}
               >
