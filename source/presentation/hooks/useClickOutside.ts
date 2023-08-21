50f441874c952da57e455156e8910802e5167393
Gabriel Jesus <gabrieljesus9221@gmail.com>
2023-08-21 19:43:15 -0300

    feat: adds use click outside hook

---
+++ source/presentation/hooks/useClickOutside.ts
@@ -0,0 +1,23 @@
+import { MutableRefObject, useEffect } from "react";
+
+export function useClickOutside<T extends HTMLElement>(
+  item: MutableRefObject<T | null>,
+  callback: () => void
+): void {
+  function handleClick({ target }: MouseEvent): void {
+    if (item.current && !item.current.contains(target as Node)) {
+      callback();
+    }
+  }
+
+  useEffect(() => {
+    document.addEventListener("mouseup", handleClick);
+    document.addEventListener("mousedown", handleClick);
+
+    return () => {
+      console.log("return");
+      document.removeEventListener("mouseup", handleClick);
+      document.removeEventListener("mousedown", handleClick);
+    };
+  });
+}
