2900c1877fe29f6f3b992cc098a27c855a2fc6c5
Gabriel Jesus <gabrieljesus9221@gmail.com>
2023-09-10 16:37:39 -0300

    refactor: improves deck statistics

--- source/presentation/components/common/DeckBuilder/DeckStatistics.tsx
+++ source/presentation/components/common/DeckBuilder/DeckStatistics.tsx
@@ -2,6 +2,7 @@ import { deckStatisticsSelector } from "@/presentation/store/genericAtoms";
 import { useRecoilValue } from "recoil";
 import { useMemo } from "react";
 import { DonutChart } from "../DonutChart";
+import classNames from "classnames";
 
 export const DeckStatistics = (): JSX.Element => {
   const totalCards = useRecoilValue(deckStatisticsSelector);
@@ -25,9 +26,23 @@ export const DeckStatistics = (): JSX.Element => {
     };
   }, [totalCards]);
 
+  const reachedTheLimit = useMemo(() => {
+    return totalCards.total === 60;
+  }, [totalCards]);
+
   return (
     <div className="bg-system shadow-md rounded-2xl w-full py-5 px-6">
       <h2 className="font-bold text-2xl text-center">Estatísticas</h2>
+      <p className="w-full text-center">
+        Total de cartas:{" "}
+        <span
+          className={classNames("font-bold",
+            reachedTheLimit ? "text-secondary" : "text-error"
+          )}
+        >
+          {totalCards.total}
+        </span>
+      </p>
       {totalCards.total !== 0 ? (
         <DonutChart data={dataset}></DonutChart>
       ) : (
