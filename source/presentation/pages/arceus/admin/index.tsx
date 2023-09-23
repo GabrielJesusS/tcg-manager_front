6ce0bd3d5615811ed51eaa580e0bf65e35a2bbe9
Gabriel Jesus <gabrieljesus9221@gmail.com>
2023-09-23 05:10:20 -0300

    chore: adds admin article list

---
+++ source/presentation/pages/arceus/admin/index.tsx
@@ -0,0 +1,20 @@
+import { AdminManagerList } from "@/presentation/components/admin/AdminManagerList";
+import { AdminSideBar } from "@/presentation/components/admin/AdminSidebar";
+import { SearchFilter } from "@/presentation/components/admin/SearchFilter";
+import { desktopOnly } from "@/presentation/middlewares/desktopOnly";
+
+function ArticleList(): JSX.Element {
+  return (
+    <div className="flex">
+      <AdminSideBar />
+      <div className="bg-green-600 w-full h-screen shrink">
+        <div className="w-full max-w-7xl mx-auto space-y-4 bg-system overflow-y-auto h-full p-safe">
+          <SearchFilter />
+          <AdminManagerList />
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default desktopOnly(ArticleList);
